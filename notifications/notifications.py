de0aafa37a647b051041aef40e5bfc1120765192
Sven Boor <sven.boor@gmail.com>
2022-06-28 15:14:10 +0200

    Improved notification.

--- notifications/notifications.py
+++ notifications/notifications.py
@@ -21,7 +21,7 @@ Dit is een automatisch verzonden bericht door het dashboard deelmobiliteit.
     """
     )
     stop = state_change.stop
-    return open_microhub.substitute(name_microhub = stop.name, url_microhub = "https://dashboarddeelmobiliteit.nl/map/zones/" + stop.stop_id,
+    return open_microhub.substitute(name_microhub = stop.name, url_microhub = "https://dashboarddeelmobiliteit.nl/map/zones/" + stop.geography_id,
         modalities = ','.join(state_change.opened))
 
 def get_close_microhub_text(state_change):
@@ -34,7 +34,7 @@ Dit is een automatisch verzonden bericht door het dashboard deelmobiliteit.
     """
     )
     stop = state_change.stop
-    return close_microhub.substitute(name_microhub = stop.name, url_microhub = "https://dashboarddeelmobiliteit.nl/map/zones/" + stop.stop_id,
+    return close_microhub.substitute(name_microhub = stop.name, url_microhub = "https://dashboarddeelmobiliteit.nl/map/zones/" + stop.geography_id,
         modalities = ','.join(state_change.closed))
         
 
